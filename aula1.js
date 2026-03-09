f8b5b206ee6ac8c2433032e0e55c8ce390a8966d
GustavoJansen <gustavo.jansen@escola.pr.gov.br>
2026-03-09 08:55:14 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,15 @@
+let nome = "Gustavo Jansen"
+let altura = 1.76
+let idade = 15
+let estado_civil = "solteiro"
+let trabalho = "Ivan auto peças"
+let emocao = "Legal"
+
+
+
+
+console.log("Fala guys, meu nome é " + nome + "! Possuo " + altura + " de altura, estou " + estado_civil + " rs rs rs, atualmente estou trabalhando na " + trabalho + ", sou " + emocao + " e gentil, valeu ai. ")
+
+
+    
+
